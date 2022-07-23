c29cbb74324931a0a12510540b5b22e2f6f4f886
hys0254 <hys0254@naver.com>
2022-07-23 20:54:19 +0900

    자잘한 버그 수정

--- crawling/src/crawling/MGBox_TopgunMV.java
+++ crawling/src/crawling/MGBox_TopgunMV.java
@@ -32,7 +32,7 @@ public class MGBox_TopgunMV {
 
 		// Driver SetUp 창 띄우기 안띄우기 headless 면 창 안띄우기
 		ChromeOptions options = new ChromeOptions();
-//		options.addArguments("headless");
+		options.addArguments("headless");
 		driver = new ChromeDriver(options);
 		// driver = new ChromeDriver();
 		// 확인할 url
@@ -42,7 +42,14 @@ public class MGBox_TopgunMV {
 
 	public static void main(String[] args) {
 		while (true) {
-			new MGBox_TopgunMV().operate();
+			
+			try {
+				new MGBox_TopgunMV().operate();
+			} catch (Exception e) {
+				telegramAlert("오류 발생 확인 요망");
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
@@ -55,8 +62,10 @@ public class MGBox_TopgunMV {
 			searchMovie();
 
 		} catch (Exception e) {
+			telegramAlert("오류 발생 확인 요망");
 			System.out.println(e);
 		} finally {
+			
 			driver.close();
 			driver.quit();
 		}
@@ -65,49 +74,50 @@ public class MGBox_TopgunMV {
 
 	public void waitforit() {
 		try {
-			Thread.sleep(1000);
+			Thread.sleep(3000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			telegramAlert("오류 발생 확인 요망");
 		}
 	}
-	
-	public void telegramAlert(String msg) {
+
+	public static void telegramAlert(String msg) {
 		// 텔레그램 메세지 전숭 부분
-					// 토큰
-					// chat_id = https://api.telegram.org/bot+[토큰]+/getUpdates 으로 확인
-					String Token = "**********************************************";
-					String chat_id = "******************************";
+		// 토큰
+		// chat_id = https://api.telegram.org/bot+[토큰]+/getUpdates 으로 확인
+		
 
-					BufferedReader in = null;
+		BufferedReader in = null;
 
-					try {
+		try {
 
-						URL obj = new URL(
-								"https://api.telegram.org/bot" + Token + "/sendmessage?chat_id=" + chat_id + "&text=" + msg); 
+			URL obj = new URL(
+					"https://api.telegram.org/bot" + Token + "/sendmessage?chat_id=" + chat_id + "&text=" + msg);
 
-						HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-						con.setRequestMethod("GET");
-						in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
-						String line;
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+			con.setRequestMethod("GET");
+			
+			in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+			String line;
 
-						while ((line = in.readLine()) != null) {
-							System.out.println(line);
-						}
+			while ((line = in.readLine()) != null) {
+				System.out.println(line);
+			}
 
-					} catch (Exception e5) {
-						e5.printStackTrace();
-					} finally {
-						if (in != null)
-							try {
-								in.close();
-							} catch (Exception e5) {
-								e5.printStackTrace();
-							}
-					}
+		} catch (Exception e5) {
+			e5.printStackTrace();
+			telegramAlert("오류 발생 확인 요망");
+		} finally {
+			if (in != null)
+				try {
+					in.close();
+				} catch (Exception e5) {
+					e5.printStackTrace();
+				}
+		}
 
 	}
-	
+
 	public void searchMovie() {
 
 		driver.get(url);
@@ -127,14 +137,14 @@ public class MGBox_TopgunMV {
 		daySelector = daySelector.findElement(By.className("wrap"));
 		List<WebElement> dayList = daySelector.findElements(By.tagName("button"));
 		for (WebElement web : dayList) {
-			if (web.getAttribute("date-data").equals("2022.07.26")) {
+			if (web.getAttribute("date-data").equals("2022.07.27")) {
 
 				daySelector = web;
 				break;
 			}
 		}
 		// 예매 오픈 탑건 존재 확인
-		if (!daySelector.getAttribute("class").equals("disabled")&&alarmCnt_ref<10) {
+		if (!daySelector.getAttribute("class").equals("disabled") && alarmCnt_ref < 10) {
 			daySelector.click();
 			waitforit();
 			WebElement movieListBox = driver
@@ -151,10 +161,7 @@ public class MGBox_TopgunMV {
 				}
 			}
 		}
-		
-		
-		
-		
+
 		// 2. 특정 날짜 자리수 변환 확인
 		// 화요일 예매 자리 확인
 		daySelector = driver
@@ -193,18 +200,19 @@ public class MGBox_TopgunMV {
 								.parseInt(time.findElement(By.className("chair")).getText().replaceAll("[^0-9]", ""));
 						System.out.println(peopleCnt);
 						if (peopleCnt != peopleCnt_19) {
-							telegramAlert("[2.1] 26일 화요일 \n19시 인원 달라짐 확인 요망!!");
+							telegramAlert("[2.1] 26일 화요일 %0a 19시 인원 달라짐 확인 요망!!");
 							peopleCnt_19 = peopleCnt;
 						}
 
 					}
+					waitforit();
 					if (time.findElement(By.className("time")).getText().equals("22:10")) {
 						scanOver = true;
 						int peopleCnt = Integer
 								.parseInt(time.findElement(By.className("chair")).getText().replaceAll("[^0-9]", ""));
 						System.out.println(peopleCnt);
 						if (peopleCnt != peopleCnt_22) {
-							telegramAlert("[2.2] 26일 화요일 \n22시 인원 달라짐 확인 요망!!");
+							telegramAlert("[2.2] 26일 화요일 %0a 22시 인원 달라짐 확인 요망!!");
 							peopleCnt_22 = peopleCnt;
 						}
 					}
